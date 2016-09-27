c3dafe46b1aad3a3abf1e0683d9877167b809915
Joaquim Esteves <joaquimbve@hotmail.com>
2016-09-27 11:55:05 +0100

    all that's needed is to actually send through TCP

--- user.py
+++ user.py
@@ -4,10 +4,13 @@ import os
 import sys
 import settings
 import argparse
+import socket
 from protocols import UDP
 from utils import Logger
+import base64
 log = Logger(debug=settings.DEBUG)
-
+TCS_NAME = "tejo" #SET THESE IN MAIN PLZ
+TCS_PORT = "12345" #SET THESE IN MAIN PLZ
 def _list(args):
     """Method that handles the list functionality"""
     udp = UDP(args.tcs_name, args.tcs_port)
@@ -32,27 +35,46 @@ def _request(input_data):
 		log.warning("You're probably not using the correct formating, please use:\n request n t W1 W2 ... WN\n or \n request n f filename ")
 	pass
 
-def _request_text(input,language):
-	number_of_words = len(input)
+
+def _find_TCP_server(language):
+	"""Find the TCP server I'm going to communicate with for the language given as a parametre"""
 	message_to_TCS = "UNQ " + LANGUAGE_ARRAY[language] + "\n"
-	#send2u #TODO
-	receivefromu = "" #TODO
-	TCS_answer= receivefromu.split() #TODO
+	try:
+		udp = UDP(TCS_NAME, TCS_PORT)
+		TCS_answer = udp.request(message_to_TCS)
+		print response
+	except:
+		log.error("Error with the bloody TCS connection mate!")
+	TCS_answer= TCS_answer.split() #TODO
 	if not TCS_answer.startswith("UNR"):
 		log.error("We got a really weird message back from the TCS server!")
-		log.info(receivefromu)
+		log.info(TCS_answer)
 	elif TCS[1] != LANGUAGE_ARRAY[language]:
 		log.error("We seem to be having a problem here, I want to speak in" + LANGUAGE_ARRAY[language] + " but the server understood it as " + TCS[1] + "\n")
+	return TCS_answer	
+	
+def _request_text(input,language):
+	"""Requesting a text translation (TCP)"""
+	number_of_words = len(input)
+	TCS_info = _find_TCP_server(language)
 	try:
-		TCPtname = TCS[2]
-		TCPport = TCS[3]
+		TCPtname = TCS_info[2]
+		TCPport = TCS_info[3]
+		message_to_TRS = "TRQ t "+ number_of_words+" " + " ".join(str(x) for x in input) + "\n"
 	except:
 		log.error("TCS server sent us a message with the wrong size. BAD TCS!")
 	pass
-	#send2u
-	#receivefromu
+	#SEND MESSAGE WITH TCP
 
-def _request_file(input,language):
+def _request_file(filename,language):
+	"""Requesting a file translation! (TCP)"""
+	try:
+		file = open(filename)
+		#WE'RE GOING TO USE BASE 64 FOR FILE TRANSFER. THIS IS REALLY IMPORTANT PAY ATTENTION!!!!!!!!!!!!!!
+		encoded_data = base64.b64encode(file.readlines())
+		filesize = len(encoded_data) #in bytes!
+		message_to_TCP = "TRQ f " + filename + " " + filesize + " " + encoded_data
+		#actually send the data through TCP to TCR
 	pass
 
 if __name__ == "__main__":
